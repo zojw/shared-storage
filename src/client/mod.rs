6ee311e8cb6e2684e893ecec85e9e0573f970cbf
zojw <robi@hey.com>
2022-02-16 17:34:55 +0800

    add local discovery abstract
    Signed-off-by: zojw <robi@hey.com>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -48,6 +48,7 @@ mod tests {
             CacheStatus, MemCacheStore, NodeBucketService, NodeCacheManager, Uploader,
         },
         client::{apipb, cli::Client},
+        discovery::{local::LocalSvcDiscovery, Discovery, ServiceType},
         error::Result,
         manifest::{
             manifestpb::{
@@ -64,9 +65,11 @@ mod tests {
     async fn it_works() -> Result<()> {
         // 0. blob_store access helper (shared lib)
         let blob_store = Arc::new(MemBlobStore::default());
+        let discovery = LocalSvcDiscovery::default();
 
         // 1. cache node above blob_store(grpc service)
-        let (cache_upload_svc, cache_read_svc, node_bucket_svc, node_cache_mng_svc) = {
+        // 1.1. setup cache-node-1 svc
+        let cache_node1_id = {
             let local_store = Arc::new(MemCacheStore::default());
             let cache_status = Arc::new(CacheStatus::new(local_store.clone()).await?);
             let uploader: Uploader<MemCacheStore, MemBlobStore, MemCacheStore> = Uploader::new(
@@ -83,57 +86,123 @@ mod tests {
             ));
             let node_cache_mng_svc =
                 NodeCacheManageServiceServer::new(NodeCacheManager::new(cache_status));
-            (upload_svc, read_svc, node_bucket_svc, node_cache_mng_svc)
+
+            let srv_id = 1;
+            discovery
+                .register_cache_node_svc(
+                    srv_id,
+                    node_cache_mng_svc,
+                    node_bucket_svc,
+                    upload_svc,
+                    read_svc,
+                )
+                .await;
+
+            srv_id
+        };
+
+        // 1.2. setup cache node-2 svc
+        let cache_node2_id = {
+            let local_store = Arc::new(MemCacheStore::default());
+            let cache_status = Arc::new(CacheStatus::new(local_store.clone()).await?);
+            let uploader: Uploader<MemCacheStore, MemBlobStore, MemCacheStore> = Uploader::new(
+                local_store.clone(),
+                blob_store.clone(),
+                None,
+                cache_status.clone(),
+            );
+            let upload_svc = BlobUploaderServer::new(uploader);
+            let read_svc = apipb::reader_server::ReaderServer::new(crate::cache::CacheReader {});
+            let node_bucket_svc = NodeBucketServiceServer::new(NodeBucketService::new(
+                local_store.clone(),
+                cache_status.clone(),
+            ));
+            let node_cache_mng_svc =
+                NodeCacheManageServiceServer::new(NodeCacheManager::new(cache_status));
+
+            let srv_id = 2;
+            discovery
+                .register_cache_node_svc(
+                    srv_id,
+                    node_cache_mng_svc,
+                    node_bucket_svc,
+                    upload_svc,
+                    read_svc,
+                )
+                .await;
+
+            srv_id
         };
 
         // 2. manifest server manage cache node and blob_store(grpc service)
-        let (manifest_status, manifest_blob_ctrl_svc, manifest_locator_svc, manifest_bucket_svc) = {
+        let (_, manifest_status) = {
             let meta_store = MemBlobMetaStore::new(blob_store.clone()).await?;
             let version_set = Arc::new(VersionSet::new(meta_store).await?);
             let blob_ctrl_svc = BlobUploadControlServer::new(BlobControl::new(version_set.clone()));
-            let node_cache_mng =
-                NodeCacheManageServiceClient::new(local_bridge(node_cache_mng_svc).await?);
-            let manifest_status = build_and_run_manifest_status(vec![HeartbeatTarget {
-                server_id: 1,
-                invoker: node_cache_mng,
-            }])
-            .await;
+
+            let node_cache_svc = discovery
+                .list(ServiceType::NodeCacheManageSvc)
+                .await?
+                .iter()
+                .map(|s| HeartbeatTarget {
+                    server_id: s.server_id,
+                    invoker: NodeCacheManageServiceClient::new(s.channel.clone()),
+                })
+                .collect::<Vec<HeartbeatTarget>>();
+            let manifest_status = build_and_run_manifest_status(node_cache_svc).await;
 
             let locator_svc = LocatorServer::new(CacheServerLocator::new(
                 version_set.clone(),
                 manifest_status.clone(),
             ));
 
-            let node_bucket = NodeBucketServiceClient::new(local_bridge(node_bucket_svc).await?);
+            let bucket_in_caches = discovery
+                .list(ServiceType::NodeBucketSvc)
+                .await?
+                .iter()
+                .map(|s| NodeBucketServiceClient::new(s.channel.clone()))
+                .collect::<Vec<NodeBucketServiceClient<Channel>>>();
             let cluster_bucket_svc = ManifestBucketServiceServer::new(BucketService::new(
                 blob_store,
                 version_set,
-                vec![node_bucket],
+                bucket_in_caches,
             ));
-            (
-                manifest_status,
-                blob_ctrl_svc,
-                locator_svc,
-                cluster_bucket_svc,
-            )
+
+            let srv_id = 3;
+            discovery
+                .register_manifest_svc(srv_id, blob_ctrl_svc, cluster_bucket_svc, locator_svc)
+                .await;
+
+            (srv_id, manifest_status)
         };
 
         // 3. client use mainifest & cache node(lib).
         {
-            let cache_uploader = BlobUploaderClient::new(local_bridge(cache_upload_svc).await?);
-            let cache_reader = ReaderClient::new(local_bridge(cache_read_svc).await?);
-            let blob_ctrl =
-                BlobUploadControlClient::new(local_bridge(manifest_blob_ctrl_svc).await?);
-            let locator = LocatorClient::new(local_bridge(manifest_locator_svc).await?);
-            let manifest_bucket =
-                ManifestBucketServiceClient::new(local_bridge(manifest_bucket_svc).await?);
-            let mut client = Client::new(
-                blob_ctrl,
-                vec![cache_uploader],
-                locator,
-                vec![cache_reader],
-                manifest_bucket,
+            let (blob_ctrl_svc, manifest_locator_svc, manifest_bucket_svc) = (
+                &discovery.list(ServiceType::ManifestBlobCtrl).await?[0],
+                &discovery.list(ServiceType::ManifestLocatorSvc).await?[0],
+                &discovery.list(ServiceType::ManifestBucketSvc).await?[0],
             );
+            let blob_ctrl = BlobUploadControlClient::new(blob_ctrl_svc.channel.clone());
+            let locator = LocatorClient::new(manifest_locator_svc.channel.clone());
+            let manifest_bucket =
+                ManifestBucketServiceClient::new(manifest_bucket_svc.channel.clone());
+
+            let (upload_svc, read_svc) = (
+                discovery
+                    .list(ServiceType::NodeUploadSvc)
+                    .await?
+                    .iter()
+                    .map(|s| BlobUploaderClient::new(s.channel.clone()))
+                    .collect::<Vec<_>>(),
+                discovery
+                    .list(ServiceType::NodeReadSvc)
+                    .await?
+                    .iter()
+                    .map(|s| ReaderClient::new(s.channel.clone()))
+                    .collect::<Vec<_>>(),
+            );
+            let mut client = Client::new(blob_ctrl, upload_svc, locator, read_svc, manifest_bucket);
 
             // 4. simple test.
             client.create_bucket("b1").await?;
--- src/lib.rs
+++ src/lib.rs
@@ -17,14 +17,6 @@
 mod blobstore;
 mod cache;
 mod client;
+mod discovery;
 mod error;
 mod manifest;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
